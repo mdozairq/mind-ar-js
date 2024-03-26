aaec1f6ec24fd86792a4437f02cdbcc5f0ae77ad
MD OZAIR QAYAM <66690229+mdozairq@users.noreply.github.com>
2024-03-26 15:50:31 +0530

    ~

--- src/main.mjs
+++ src/main.mjs
@@ -1,10 +1,36 @@
 import * as  functions from '@google-cloud/functions-framework';
 import * as https from 'https';
 import { OfflineCompiler } from './image-target/offline-compiler.js';
-
 import { loadImage } from 'canvas';
-functions.http('helloHttp', (req, res) => {
+const { Storage } = require('@google-cloud/storage');
+
+async function uploadToGCS(file_location, bucket_name, upload_prefix, final_output_name) {
+  return new Promise(async (resolve, reject) => {
+      //console.log("Uploading", bucket_name, upload_prefix, final_output_name);
+      const storage = new Storage();
+
+      const bucket = storage.bucket(bucket_name);
+
+      try {
+          const destinationPath = `${upload_prefix}`
+
+          await bucket.upload(file_location, {
+              destination: destinationPath,
+          });
+
+          const fileUploadedUrl = `https://storage.googleapis.com/${bucket_name}/${destinationPath}`;
+          resolve(fileUploadedUrl);
+      } catch (err) {
+          console.error('Error uploading image to GCS:', err);
+          reject(err);
+      }
+  })
+}
+
+
+functions.http('helloHttp', async (req, res) => {
     let imgUrl = req.query.img
+    let objectId = req.query.id
     https.get(imgUrl, (response) => {
         const chunks = [];
       
@@ -19,14 +45,17 @@ functions.http('helloHttp', (req, res) => {
             await compiler.compileImageTargets([images], console.log);
             const featureBuffer = compiler.exportData();
             console.log('Feature buffer:', featureBuffer);
-            const filename = 'target.mind';
+            const filename = `${objectId}.mind`;
 
             // Set headers
-            res.set('Content-Disposition', `attachment; filename="${filename}"`);
-            res.set('Content-Length', featureBuffer.length);
-            res.send(featureBuffer);
+            // res.set('Content-Disposition', `attachment; filename="${filename}"`);
+            // res.set('Content-Length', featureBuffer.length);
+            // res.send(featureBuffer);
         });
       }).on('error', (err) => {
         console.error('Error fetching image:', err);
       });
+
+      let mindFileUrl = await uploadToGCS()
+
 });
